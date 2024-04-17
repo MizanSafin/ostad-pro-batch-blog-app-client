1efadb62f35985aac83307e1c388f57942b5f03d
MizanSafin <drmizanurrahman452@gmail.com>
2024-04-18 00:58:16 +0600

    fetch comment data from comment Api route

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,11 +1,41 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function CommentSection({ postId }) {
   const [comment, setComment] = useState("");
+  const [commentErr, setCommentErr] = useState(null);
   const { currentUser } = useSelector((state) => state.user);
+  const [comments, setComments] = useState([]);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (comment.trim() === "") {
+        return;
+      }
+      let reqBody = { content: comment, postId, userId: currentUser._id };
+
+      axios
+        .post(`http://localhost:3232/api/v1/comment/create-comment`, reqBody, {
+          withCredentials: true,
+        })
+        .then((res) => {
+          if (res.data.success === true) {
+            setCommentErr(null);
+            setComment("");
+            setComments([...comments, res.data.comment]);
+          }
+        })
+        .catch((err) => {
+          setCommentErr(err.message);
+        });
+    } catch (error) {
+      setCommentErr(error.message);
+    }
+  };
+  console.log(comments);
   return (
     <div className=" mb-5">
       {currentUser ? (
@@ -39,7 +69,10 @@ function CommentSection({ postId }) {
 
       {currentUser && (
         <>
-          <form className="max-w-2xl mx-auto w-full border p-1 border-lime-300">
+          <form
+            onSubmit={handleSubmit}
+            className="max-w-2xl mx-auto w-full border p-1 border-lime-300"
+          >
             <textarea
               placeholder="Add a comment"
               name="comment"
@@ -54,7 +87,10 @@ function CommentSection({ postId }) {
               <h2 className="text-xs">
                 Remaining letters : {120 - comment.length}
               </h2>
-              <button className="bg-lime-300 text-lime-700 px-4 py-1 rounded-md hover:bg-lime-700 hover:text-lime-300 transition-colors">
+              <button
+                type="submit"
+                className="bg-lime-300 text-lime-700 px-4 py-1 rounded-md hover:bg-lime-700 hover:text-lime-300 transition-colors"
+              >
                 Submit
               </button>
             </div>
